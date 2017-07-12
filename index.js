7608d9be6562aa4379881b30e26920bac0bc0b71
Armen Vardanyan <armen.vardanyan@basiccrm.am>
2017-07-12 20:55:24 +0400

    aaa

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev')); //before router
 app.use("/", router);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
 app.listen(3000, () => {
     console.log("Server started on port 3000 ...")
 });
